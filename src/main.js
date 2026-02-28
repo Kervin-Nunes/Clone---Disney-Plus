41425e74aafc0dac07631bd3a8bfc404544c974c
DeluxeCalango <kervin0santos@gmail.com>
2026-02-28 00:05:46 -0300

    Ajustes No Mobile

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
+    const header = document.querySelector('.header');
+
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function() {
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < alturaHero) {
+            header.classList.add('header--is-hidden');
+        } else {
+            header.classList.remove('header--is-hidden');
+        }
+    })
     
-    
+    // Seção das atrações, programação das abas
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener("click", function(botoao) {
             const abaAlvo = botoao.target.dataset.tabButton;
@@ -12,7 +27,19 @@ document.addEventListener("DOMContentLoaded", function() {
             botoao.target.classList.add('shows__tabs__button--is--active');
         })
     }
+
+    // Seção FAQ, accordion das perguntas
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener("click", abreOuFechaResposta);
+    }
 })
+
+function abreOuFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+}
 
 function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]');
